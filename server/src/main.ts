4f2582017ca391e8dea35ad8d8ee0eb043d17006
IhorLukin <igor.lukin.13171@gmail.com>
2024-03-17 20:34:18 +0200

    another change

--- server/src/main.ts
+++ server/src/main.ts
@@ -10,6 +10,6 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
 });
-  await app.listen(3333);
+  await app.listen(process.env.PORT);
 }
 bootstrap();
